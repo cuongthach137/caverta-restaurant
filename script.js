fd7ad898f79ff04fcdbd5d985a0e8659e1772ddb
Cuong Thach <81078285+cuongthach137@users.noreply.github.com>
2021-04-21 00:47:33 +0700

    Update script.js

--- script.js
+++ script.js
@@ -6,38 +6,27 @@ const intervalTime = 5000;
 let slideInterval;
 
 const nextSlide = () => {
-  // Get current class
   const current = document.querySelector('.current');
-  // Remove current class
   current.classList.remove('current');
-  // Check for next slide
   if (current.nextElementSibling) {
-    // Add current to next sibling
     current.nextElementSibling.classList.add('current');
   } else {
-    // Add current to start
     slides[0].classList.add('current');
   }
   setTimeout(() => current.classList.remove('current'));
 };
 
 const prevSlide = () => {
-  // Get current class
   const current = document.querySelector('.current');
-  // Remove current class
   current.classList.remove('current');
-  // Check for prev slide
   if (current.previousElementSibling) {
-    // Add current to prev sibling
     current.previousElementSibling.classList.add('current');
   } else {
-    // Add current to last
     slides[slides.length-1].classList.add('current');
   }
   setTimeout(() => current.classList.remove('current'));
 };
 
-// Button events
 next.addEventListener('click', e => {
   nextSlide();
   if (auto) {
@@ -54,8 +43,17 @@ prev.addEventListener('click', e => {
   }
 });
 
-// Auto slide
 if (auto) {
-  // Run next slide at interval time
   slideInterval = setInterval(nextSlide, intervalTime);
 }
+
+const navBarMain = document.querySelector(".nav-bar-main");
+window.addEventListener('scroll', function(){
+  if (window.scrollY > navBarMain.getBoundingClientRect().height){
+    navBarMain.classList.add('fixed')
+  }else{
+    navBarMain.classList.remove('fixed')
+
+  }
+
+})
